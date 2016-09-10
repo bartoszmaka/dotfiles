f17818a3d4a13c02bd08a8a5a4883c8280ab5d22
bartoszmaka <moncznybartosz@gmail.com>
2016-09-10 22:57:17 +0200

    updated ubuntu post installation script

---
+++ ubuntu_post_install.sh
@@ -0,0 +1,11 @@
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo add-apt-repository ppa:numix/ppa
+sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository ppa:umang/indicator-stickynotes
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install software-properties-common python-software-properties build-essential cmake gcc make exuberant-ctags python-dev python3-dev python-pip python3-pip zsh tmux git wget redshift redshift-gtk indicator-multiload indicator-cpufreq vlc bless gparted font-manager fonts-hack-ttf unity-tweak-tool compizconfig-settings-manager gimp inkscape psensor silversearcher-ag unrar xclip neovim simplescreenrecorder grub-customizer numix-icon-theme-circle numix-icon-theme yosembiance-gtk-theme
