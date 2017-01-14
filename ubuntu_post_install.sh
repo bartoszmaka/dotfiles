2c0e532a76672c7f03b10ad61a495fb7833649e9
bartoszmaka <moncznybartosz@gmail.com>
2017-01-14 16:11:00 +0100

    checklist log fix

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -43,6 +43,7 @@ install_grub_customizer() {
 }
 
 install_neovim() {
+    clear
     echo "installing neovim requirements and dependencies"
     sudo apt install -y software-properties-common python-dev python-pip python3-dev python3-pip exuberant-ctags xclip make cmake gcc silversearcher-ag
     echo "installing neovim"
@@ -56,9 +57,9 @@ install_neovim() {
     sudo -H pip3 install --upgrade neovim
 
     echo "installing vim-plug"
-    echo "vim-plug" > ~/checklist
-    echo "https://github.com/junegunn/vim-plug" > ~/checklist
-    echo "after you install rvm -> `gem install neovim`" > ~/checklist
+    echo "vim-plug" >> ~/checklist
+    echo "https://github.com/junegunn/vim-plug" >> ~/checklist
+    echo "after you install rvm ->> `gem install neovim`" > ~/checklist
 
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -71,38 +72,38 @@ install_neovim() {
     ln -s ~/.vimrc ~/.config/nvim/init.vim
 }
 fill_checklist() {
-    echo "numix icons" > ~/checklist
-    echo "add-apt-repository ppa:numix/ppa" > ~/checklist
-    echo "gtk theme" > ~/checklist
-    echo "add-apt-repository ppa:noobslab/themes" > ~/checklist
-    echo "enable 24 bit colors in tmux" > ~/checklist
-    echo "https://sunaku.github.io/tmux-24bit-color.html#usage" > ~/checklist
-    echo "oh-my-zsh" > ~/checklist
-    echo "https://github.com/robbyrussell/oh-my-zsh" > ~/checklist
-    echo "change console bg to #282C34" > ~/checklist
-    echo "change console font color to #A7AEBB" > ~/checklist
-    echo "check zshrc" > ~/checklist
-    echo "spotify" > ~/checklist
-    echo "https://www.spotify.com/pl/download/linux/" > ~/checklist
-    echo "virtualbox" > ~/checklist
-    echo "https://www.virtualbox.org/wiki/Downloads" > ~/checklist
-    echo "messenger" > ~/checklist
-    echo "https://github.com/Aluxian/Messenger-for-Desktop/releases" > ~/checklist
-    echo "rvm" > ~/checklist
-    echo "https://rvm.io/rvm/install" > ~/checklist
+    echo "numix icons" >> ~/checklist
+    echo "add-apt-repository ppa:numix/ppa" >> ~/checklist
+    echo "gtk theme" >> ~/checklist
+    echo "add-apt-repository ppa:noobslab/themes" >> ~/checklist
+    echo "enable 24 bit colors in tmux" >> ~/checklist
+    echo "https://sunaku.github.io/tmux-24bit-color.html#usage" >> ~/checklist
+    echo "oh-my-zsh" >> ~/checklist
+    echo "https://github.com/robbyrussell/oh-my-zsh" >> ~/checklist
+    echo "change console bg to #282C34" >> ~/checklist
+    echo "change console font color to #A7AEBB" >> ~/checklist
+    echo "check zshrc" >> ~/checklist
+    echo "spotify" >> ~/checklist
+    echo "https://www.spotify.com/pl/download/linux/" >> ~/checklist
+    echo "virtualbox" >> ~/checklist
+    echo "https://www.virtualbox.org/wiki/Downloads" >> ~/checklist
+    echo "messenger" >> ~/checklist
+    echo "https://github.com/Aluxian/Messenger-for-Desktop/releases" >> ~/checklist
+    echo "rvm" >> ~/checklist
+    echo "https://rvm.io/rvm/install" >> ~/checklist
 }
 clone_dotfiles() {
     # I assume, this file was downloaded from my repo to ~/dotfiles
     clear
-    echo "redshift.conf -> ~/.config/"
+    echo "redshift.conf ->> ~/.config/"
     cp redshift.conf ~/.config/
-    echo ".vimrc -> ~/"
+    echo ".vimrc ->> ~/"
     cp .vimrc ~/
-    echo ".rubocop.yml -> ~/"
+    echo ".rubocop.yml ->> ~/"
     cp .rubocop.yml ~/
-    echo ".tmux.conf -> ~/"
+    echo ".tmux.conf ->> ~/"
     cp .tmux.conf ~/
-    echo "zsh theme robbyrussell -> agnoster"
+    echo "zsh theme robbyrussell ->> agnoster"
     sed -i -e 's/robbyrussell/agnoster/g' ~/.zshrc
 }
 
@@ -114,10 +115,13 @@ install_multimedia
 install_grub_customizer
 install_simplescreenrecorder
 install_neovim
-clone_dotfiles
 
 echo "Downloading chrome..."
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+clone_dotfiles
+
+echo "Done, you should reboot now"
