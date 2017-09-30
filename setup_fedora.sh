c2fe291c5f02d6b4ae0c2928ce308d54aa31651f
Bartosz <bartosz@Bartoszs-iMac.local>
2017-09-30 20:36:34 +0200

    Add setup scripts for osx ubuntu and fedora

---
+++ setup_fedora.sh
@@ -0,0 +1,6 @@
+echo 'Running setup for fedora'
+echo 'copying config files'
+cp -v ~/repos/dotfiles/linux/.tmux.conf ~/
+cp -v ~/repos/dotfiles/.vimrc ~/
+cp -v ~/repos/dotfiles/.rubocop.yml ~/
+cp -v ~/repos/dotfiles/redshift.conf ~/.config/
