3204a67b0fd9b799fe419e96ba85e8fe895eef6f
bartoszmaka <bartoszmaka95@gmail.com>
2017-11-08 17:47:34 +0100

    Reorganize files

--- setup_ubuntu.sh
+++
@@ -1,9 +0,0 @@
-echo 'Running setup for ubuntu'
-echo 'copying config files'
-cp -v ~/repos/dotfiles/linux/.tmux.conf ~/
-cp -v ~/repos/dotfiles/.vimrc ~/
-cp -v ~/repos/dotfiles/.rubocop.yml ~/
-cp -v ~/repos/dotfiles/redshift.conf ~/.config/
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y zsh silversearcher-ag python-dev python3-dev python-pip python3-pip libncurses5 libncurses5-dev
