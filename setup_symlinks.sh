1b87e04ed7f79f519d66b6c564ae130db7679632
bartoszmaka <bartoszmaka95@gmail.com>
2018-02-18 08:44:33 +0100

    distinguish between linux and osx

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,8 +1,39 @@
-ln -s noplugin_vimrc ~/.noplugin_vimrc
-ln -s vimrc ~/.vimrc
-ln -s vimrc ~/.config/nvim/init.vim
-ln -s ~/.vim ~/.config/nvim/.vim
-ln -s global_gitignore ~/.gitignore
+function download_repos {
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  git clone https://github.com/universal-ctags/ctags ~/.repos
+  git clone https://github.com/uptech/alt ~/.repos
+  git clone https://github.com/tmux/tmux ~/.repos
 
-ln -s kitty.conf ~/Library/Preferences/kitty/kitty.conf
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
 
+case "$(uname -s)" in
+
+   Darwin)
+     md -p ~/.config/nvim
+     md -p ~/.vim
+
+     ln -s noplugin_vimrc ~/.noplugin_vimrc
+     ln -s vimrc ~/.vimrc
+     ln -s vimrc ~/.config/nvim/init.vim
+     ln -s ~/.vim ~/.config/nvim/.vim
+     ln -s global_gitignore ~/.gitignore
+     download_repos
+     ;;
+
+   Linux)
+     md -p ~/.config/nvim
+     md -p ~/.vim
+     md -p ~/.fonts
+
+     ln -s ~/.config/nvim/init.vim vimrc
+     ln -s ~/.config/nvim/.vim ~/.vim
+     ln -s ~/.gitignore global_gitignore
+     ln -s ~/.rubocop.yml rubocop.yml
+     ln -s ~/.config/redshift.conf linux/redshift.conf
+     download_repos
+     ;;
+esac
