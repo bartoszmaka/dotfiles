b32223055815ad7005b763a406c7fe554fb65362
Bartosz Mąka <bartoszmaka95@gmail.com>
2020-02-06 08:50:32 +0100

    Add sample js file to repo

---
+++ vim/sampleFiles/javascriptreact.js
@@ -0,0 +1,38 @@
+import React, { Component, useState } from 'react'
+
+export class SampleClass extends Component {
+  componentDidMount() {
+    console.log('mounted')
+  }
+
+  renderFoos = (foos) =>
+    foos.map(foo => <div>{foo}</div>)
+
+  render() {
+    return (
+      <div>
+        <SampleComponent />
+      </div>
+    )
+  }
+}
+
+const SampleComponent = ({
+  name,
+  id,
+  onClick,
+  onClose,
+}) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleSomething = () => {}
+
+  return (
+    <>
+      <h2>{name}</h2>
+      <button type="button" onClick={onClick}></button>
+    </>
+  )
+}
+
+export default SampleClass
