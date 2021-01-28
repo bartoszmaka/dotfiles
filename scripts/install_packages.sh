8d56be41937c0cc66554b76830061e6238f5bce5
Bartosz Mąka <bartoszmaka95@gmail.com>
2021-01-28 18:41:41 +0100

    Update install script

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,11 +1,14 @@
 case "$(uname -s)" in
   Darwin)
-      brew install python3 the_silver_searcher neovim reattach-to-user-namespace tmux diff-so-fancy asdf gnupg coreutils curl git 
-      brew cask install alacritty spectacle flux slack karabiner-elements lunar whatsapp iterm2 goofy istat-menus alfred spotify ripgrep contexts intel-power-gadget
+      brew install the_silver_searcher neovim reattach-to-user-namespace tmux diff-so-fancy asdf gnupg coreutils curl git ripgrep git-delta imagemagick
+      brew cask install alacritty spectacle flux slack karabiner-elements lunar whatsapp iterm2 goofy istat-menus alfred spotify contexts intel-power-gadget steam
+      brew install --HEAD universal-ctags/universal-ctags/universal-ctags
       asdf plugin-add postgres
       asdf plugin-add ruby
       asdf plugin-add python
-      asdf plugin-add node
+      asdf plugin-add nodejs
+      asdf plugin-add rust
+      asdf plugin-add redis
     ;;
 
   Linux)
