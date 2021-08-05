ced21344dea5f83373c6587b0143959f4a9790d4
Bartosz Mąka <bartoszmaka95@gmail.com>
2021-08-05 11:42:08 +0200

    Reorganise repo

--- resources/scripts/setup_symlinks.sh
+++ resources/scripts/setup_symlinks.sh
@@ -1,24 +1,25 @@
 mkdir -p ~/.config/ranger
 mkdir -p ~/.config/nvim
-mkdir -p ~/.config/coc
 mkdir -p ~/.config/alacritty
 
-# ln -vsf ~/.repos/dotfiles/vim/vimrc ~/.config/nvim/init.vim
+# vim files
 ln -vsf ~/.repos/dotfiles/vim/init.lua ~/.config/nvim/
 ln -vsf ~/.repos/dotfiles/vim/lua ~/.config/nvim/
+ln -vsf ~/.repos/dotfiles/vim/UltiSnips ~/.config/nvim/
+
+ln -vsf ~/.repos/dotfiles/zshrc ~/.zshrc
 ln -vsf ~/.repos/dotfiles/resources/global_gitignore ~/.gitignore
 ln -vsf ~/.repos/dotfiles/resources/gitconfig ~/.gitconfig
-ln -vsf ~/.repos/dotfiles/zshrc ~/.zshrc
 ln -vsf ~/.repos/dotfiles/resources/rubocop.yml ~/.rubocop.yml
 ln -vsf ~/.repos/dotfiles/resources/pryrc ~/.pryrc
-ln -vsf ~/.repos/dotfiles/asdfrc ~/.asdfrc
-ln -vsf ~/.repos/dotfiles/tmux ~/
-ln -vsf ~/.repos/dotfiles/vim/UltiSnips ~/.config/nvim/
-ln -vsf ~/.repos/dotfiles/vim/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -vsf ~/.repos/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -vsf ~/.repos/dotfiles/jetbrains/ideavimrc ~/.ideavimrc
+
 ln -vsf ~/.repos/dotfiles/asdf/default-npm-packages ~/.default-npm-packages
 ln -vsf ~/.repos/dotfiles/asdf/default-gems ~/.default-gems
+ln -vsf ~/.repos/dotfiles/asdfrc ~/.asdfrc
+
+ln -vsf ~/.repos/dotfiles/tmux ~/
 ln -vsf ~/.repos/dotfiles/tmuxinator ~/
 
 case "$(uname -s)" in
