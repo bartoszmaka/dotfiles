e7459ebf163bf3d5fc91cf62dda7ff70457cd252
Bartosz Mąka <bartoszmaka95@gmail.com>
2022-09-29 09:51:46 +0200

    Make side panels darker

--- vim/lua/plugins/colorscheme.lua
+++ vim/lua/plugins/colorscheme.lua
@@ -2,11 +2,18 @@ local colors = require('config_helper.colors').onedark
 
 vim.o.termguicolors = true
 vim.o.background = "dark"
-vim.cmd "syntax on"
+-- vim.cmd "syntax on"
 require('onedark').setup({
   style = 'deep',
   term_colors = 'false',
   colors = colors,
+  code_style = {
+      comments = 'italic',
+      keywords = 'italic',
+      functions = 'none',
+      strings = 'none',
+      variables = 'none'
+  },
   highlights = {
     DiffChange = { bg=colors.diff_change, fg="none" },
     DiffText   = { bg=colors.diff_text, fg="none" },
@@ -19,7 +26,6 @@ require('onedark').load()
 vim.cmd[[
   augroup graphql_syntax_fix
     autocmd!
-  
     autocmd FileType javascript highlight! link graphqlStructure Constant
     autocmd FileType javascript highlight! link graphqlVariable Type
     autocmd FileType javascript highlight! link graphqlName String
